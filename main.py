cd72141ecd13f00173fbf3e97eee216222deb04e
sarvesh <sarvesh2459@gmail.com>
2021-08-20 22:56:30 +0530

    f

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import random
-order = ['A' , 'K' , 'Q' , 'J' , 10 , 9 , 8 , 7 , 6 , 5 ,4 ,3, 2]
-def startmatch(start , turn , cards , seat):
-    print("Start " , start)
-    print("Turn " , turn )
+
+def startmatch(start , cards):
+    order = ['A' , 'K' , 'Q' , 'J' , 10 , 9 , 8 , 7 , 6 , 5 ,4 ,3, 2]
     cnt = 0
-    seat = seat + seat
-    while cnt<4:
-        ind = seat.index(start)
-        for card in cards[seat[ind]]:
-            for i in order:
-                if card[1] == i:
-                    
+    if start != "Player":
+        for i in order:
+            for j in cards[start]:
+                if j[1] == i:
+                    return j
+    else:
+        return input()
 
--- shuffle.py
+++ shuffle.py
@@ -1,9 +1,17 @@
-import random
-def distribute(deck_list , name):
-    return {" Bot1" : deck_list[0:13] , " Bot2" : deck_list[13:26] , " Bot3" : deck_list[26:39] , name : deck_list[39:52]}
+import random , main
+def distribute(deck_list ):
+    return {"Bot1" : deck_list[0:13] , "Bot2" : deck_list[13:26] , "Bot3" : deck_list[26:39] , "Player" : deck_list[39:52]}
 deck = open('deck.txt' , 'r')
 deck_list = deck.readline().split()
 random.shuffle(deck_list)
 seat = deck.readline().split()
+print(deck_list[0:13])
+print(deck_list[13:26])
+print(deck_list[26:39])
+print(deck_list[39:52])
 random.shuffle(seat)
 print(seat)
+cnt = 0
+while cnt < 4:
+    print(main.startmatch(seat[cnt], distribute(deck_list)))
+    cnt += 1
