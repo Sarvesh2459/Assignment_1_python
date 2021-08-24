fb70de5ca917f2ede1d2924549d5be2787f82e1b
sarvesh <sarvesh2459@gmail.com>
2021-08-24 22:30:01 +0530

    f3

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import random
 def lowest(curr , cards ,  series = ''):
-    order = [2 , 3, 4 , 5 , 6 , 7 , 8, 9 ,10 , "J" , 'Q' , "K" ,"A"]
+    order = ['2' , '3', '4' , '5' , '6' , '7' , '8', '9' ,'10' , 'J' , 'Q' , 'K' ,'A']
     cnt = 0
     if curr != "Player":
         for i in order:
             for j in cards[curr]:
-                if j[1] == i :
+                if j[1:] == i :
                     return j[0] , j
     else:
         move = input()
         return move[0] , move
 def currmatch(curr , cards ,  series = ''):
-    order = ['A' , 'K' , 'Q' , 'J' , 10 , 9 , 8 , 7 , 6 , 5 ,4 ,3, 2]
+    order = ['A' , 'K' , 'Q' , 'J' , '10' , '9' , '8' , '7' , '6' , '5' ,'4' ,'3', '2']
     cnt = 0
     if curr != "Player":
         for i in order:
             for j in cards[curr]:
-                if j[1] == i :
+                if j[1:] == i :
                     return j[0] , j
     else:
         move = input()
