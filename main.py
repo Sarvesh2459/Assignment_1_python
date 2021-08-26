136bb6ae5d234c57cf3a3dc2974296d576790d10
sarvesh <sarvesh2459@gmail.com>
2021-08-27 01:34:48 +0530

    almost

--- main.py
+++ main.py
@@ -1,42 +1,67 @@
-import random
+import random, main
 def lowest(curr , cards ,  series = ''):
     order = ['2' , '3', '4' , '5' , '6' , '7' , '8', '9' ,'10' , 'J' , 'Q' , 'K' ,'A']
-    if series!='':
-        if curr != "Player":
-            for i in order:
-                for j in cards[curr]:
-                    if j[1:] == i :
-                        return j[0] , j
-        else:
-            move = input()
-            return move[0] , move
-    else:
-        if curr != "Player":
-            for i in order:
-                for j in cards[curr]:
-                    if j[1:] == i and j[0] == series:
-                        return j[0] , j
-        else:
-            move = input()
-            return move[0] , move
-def currmatch(curr , cards , first_val , series = '' ):
-    order = ['A' , 'K' , 'Q' , 'J' , '10' , '9' , '8' , '7' , '6' , '5' ,'4' ,'3', '2']
     if (not(series)):
         if curr != "Player":
             for i in order:
                 for j in cards[curr]:
                     if j[1:] == i :
-                        return j[0] , j
+                        return  j
         else:
             move = input()
-            return move[0] , move
+            return  move
     else:
         if curr != "Player":
             for i in order:
                 for j in cards[curr]:
                     if j[1:] == i and j[0] == series:
-                        return j[0] , j
+                        return  j
         else:
             move = input()
-            return move[0] , move
+            return move
+    for i in order:
+        for j in cards[curr]:
+            if j[1:] == i :
+                return  j
+def currmatch(curr , cards , series = '' ):
+    order = ['A' , 'K' , 'Q' , 'J' , '10' , '9' , '8' , '7' , '6' , '5' ,'4' ,'3', '2']
+    orderrev = ['2' , '3', '4' , '5' , '6' , '7' , '8', '9' ,'10' , 'J' , 'Q' , 'K' ,'A']
+    if (not(series)):
+        if curr != "Player":
+            for i in order:
+                for j in cards[curr]:
+                    if j[1:] == i :
+                        return j
+        else:
+            move = input()
+            return move
+    else:
+        if curr != "Player":
+            for i in order:
+                for j in cards[curr]:
+                    if j[1:] == i and j[0] == series:
+                        return j
+        else:
+            move = input()
+            return move
+    for i in orderrev:
+        for j in cards[curr]:
+            if j[1:] == i :
+                return  j
 
+def checkcard(player , cards ,card1 ,card2):
+    order = ['2' , '3', '4' , '5' , '6' , '7' , '8', '9' ,'10' , 'J' , 'Q' , 'K' ,'A']
+    if player!= "Player" :
+        if order.index(card2[1:]) > order.index(card1[1:]):
+            return card2
+        else:
+            return main.lowest(player, cards , card2[0] )
+    else :
+        return card2
+
+def winner(moves):
+    list1=['A','K','Q','J','10','9','8','7','6','5','4','3','2']
+    for j in list1:
+        for i in moves:
+            if moves[i][1:]==j:
+                return i
--- shuffle.py
+++ shuffle.py
@@ -1,17 +1,46 @@
 import random , main
+
 def distribute(deck_list ):
+    random.shuffle(deck_list)
     return {"Bot1" : deck_list[0:13] , "Bot2" : deck_list[13:26] , "Bot3" : deck_list[26:39] , "Player" : deck_list[39:52]}
 deck = open('deck.txt' , 'r')
 deck_list = deck.readline().split()
-random.shuffle(deck_list)
 seat = deck.readline().split()
-print(deck_list[0:13])
-print(deck_list[13:26])
-print(deck_list[26:39])
-print(deck_list[39:52])
+cards = distribute(deck_list)
+# order = ['2' , '3', '4' , '5' , '6' , '7' , '8', '9' ,'10' , 'J' , 'Q' , 'K' ,'A']
 random.shuffle(seat)
 print(seat)
-cnt = 0
-while cnt < 4:
-    print(main.currmatch(seat[cnt], distribute(deck_list) , series="S"))
-    cnt += 1
+seat = seat + seat
+cont = "Y"
+turn = 12
+winner = seat[0]
+wins = {}
+while cont == "Y" or cont == "y" :
+    moves = {}
+    print("Turn : " ,turn)
+    print()
+    for j in cards:
+        print(cards[j])
+    print()
+    card0 = main.currmatch(winner, cards, series = '' )
+    print( winner ,  card0)
+    cards[winner].remove(card0)
+    moves[winner] = card0
+    cnt = seat.index(winner)
+    print(cnt)
+    for i in range(1,4):
+        cnt += 1
+        card = main.currmatch(seat[cnt] ,cards , card0[0])
+        card = main.checkcard(seat[cnt] , cards ,card0 ,card)
+        print(seat[cnt] , card)
+        cards[seat[cnt]].remove(card)
+        moves[seat[cnt]] = card
+    winner = main.winner(moves)
+    print()
+    print(winner)
+    if turn == 13:
+        cont = input("Do you want to continue? Y/N : ")
+        cards = distribute(deck_list)
+        random.shuffle(seat)
+    turn +=1
+    print()
