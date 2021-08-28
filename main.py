4f5d78cafc8dd014d647deae3e2efba1d68b33fb
sarvesh <sarvesh2459@gmail.com>
2021-08-28 15:18:47 +0530

    almost

--- main.py
+++ main.py
@@ -59,9 +59,38 @@ def checkcard(player , cards ,card1 ,card2):
     else :
         return card2
 
-def winner(moves):
+def winner(moves,series):
+
     list1=['A','K','Q','J','10','9','8','7','6','5','4','3','2']
     for j in list1:
         for i in moves:
             if moves[i][1:]==j:
-                return i
+                if(series[0]==moves[i][0]):
+                    return i
+                
+def score (dict1,dict2):
+    dict3={}
+    print(dict1)
+    print(dict2)
+    for i in dict1  :
+        
+        if dict1[i] > dict2[i] :
+            point = -(10)*(dict1[i])
+        else :
+            point = 10*(dict1[i]) + (dict2[i]-dict1[i])
+        dict3[i]=point
+    return dict3
+
+def call_players(cards):
+    dict2=dict()
+    
+    for j in cards:
+        if(j=="Player"):
+            call=int(input("enter your call"))
+        else:
+            call=0
+            for i in cards[j]:
+                if(i=="HA" or i=="HK" or i=="HQ" or i=="HJ" or i=="DA" or i=="DK" or i=="DQ" or i=="DJ" or i=="SA" or i=="SK" or i=="SQ" or i=="SJ" or i=="CA" or i=="CK" or i=="CQ" or i=="CJ"):
+                    call+=1
+        dict2[j]=call
+    return dict2
--- shuffle.py
+++ shuffle.py
@@ -1,20 +1,26 @@
 import random , main
 
-def distribute(deck_list ):
-    random.shuffle(deck_list)
-    return {"Bot1" : deck_list[0:13] , "Bot2" : deck_list[13:26] , "Bot3" : deck_list[26:39] , "Player" : deck_list[39:52]}
-deck = open('deck.txt' , 'r')
-deck_list = deck.readline().split()
-seat = deck.readline().split()
-cards = distribute(deck_list)
+def distribute(on):
+    deck = open('deck.txt' , 'r')
+    deck_list = deck.readline().split()
+    seat = deck.readline().split()
+    if on=='Y' or on=='y':
+        random.shuffle(deck_list)
+        random.shuffle(seat)
+    return {"Bot1" : deck_list[0:13] , "Bot2" : deck_list[13:26] , "Bot3" : deck_list[26:39] , "Player" : deck_list[39:52]} , seat
+
+on = input('Do you want to read cards from file? Y/N')
+cards , seat = distribute(on)
 # order = ['2' , '3', '4' , '5' , '6' , '7' , '8', '9' ,'10' , 'J' , 'Q' , 'K' ,'A']
-random.shuffle(seat)
+
 print(seat)
 seat = seat + seat
 cont = "Y"
-turn = 12
+turn = 1
 winner = seat[0]
-wins = {}
+wins = {'Bot1' : 0 , 'Bot2' : 0 , 'Bot3' : 0 , 'Player' : 0}
+calls = main.call_players(cards)
+print(calls)
 while cont == "Y" or cont == "y" :
     moves = {}
     print("Turn : " ,turn)
@@ -35,12 +41,22 @@ while cont == "Y" or cont == "y" :
         print(seat[cnt] , card)
         cards[seat[cnt]].remove(card)
         moves[seat[cnt]] = card
-    winner = main.winner(moves)
+    winner = main.winner(moves,card0[0])
     print()
     print(winner)
+    wins[winner] +=1
     if turn == 13:
+        scores=main.score (calls,wins) 
+        print(scores)
+        max_score = 0
+        for i in scores:
+            if scores[i]>max_score:
+                max_score=scores[i]
+                win = i
+        print(i)
         cont = input("Do you want to continue? Y/N : ")
-        cards = distribute(deck_list)
+        turn = 1
+        cards = distribute(on)
         random.shuffle(seat)
     turn +=1
     print()
